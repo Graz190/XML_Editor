bebf442264071c7f6acd392954ff4db56b7b65ca
Christian Fagherazzi <1820548@stud.hs-mannheim.de>
2023-06-26 14:13:11 +0200

    write Replacer Method

--- XMLEditor.cs
+++ XMLEditor.cs
@@ -1,5 +1,26 @@
-using System
+using Internal;
+using System.Reflection.Metadata;
+using System.Xml.XPath;
+using System.Xml;
 class XMLEditor
 {
-    
+    public boolean runReplacer()
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXML("C:\\Users\\NCTR Admin\\Desktop\\zieldatei.xml");
+        XmlNode root = doc.DocumentElement;
+        XPathNavigator navigator = doc.CreateNavigator();
+        foreach (var nav in navigator.Select("GUID"))
+        {
+            nav.SetValue("");
+            Console.WriteLine("test")
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        XMLEditor editor = new XMLEditor();
+        editor.runReplacer();
+        
+    }
 }
